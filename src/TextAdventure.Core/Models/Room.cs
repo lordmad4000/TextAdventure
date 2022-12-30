9903fd1f08f4e80bc24b4a648eb8b5150656de06
joseantonio <lordmad4000@hotmail.com>
2022-12-30 18:26:41 +0100

    Add property Exits to Room.

--- src/TextAdventure.Core/Models/Room.cs
+++ src/TextAdventure.Core/Models/Room.cs
@@ -2,16 +2,18 @@
 {
     public class Room
     {
-        public Room(int id, string name, string description)
+        public Room(int id, string name, string description, List<Exit> exits)
         {
             Id = id;
             Name = name;
             Description = description;
+            Exits = exits;
         }
 
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public List<Exit> Exits { get; set; }
 
     }
 }
