6f32b725b084574d6b42c87037da1b0cd2221e40
joseantonio <lordmad4000@hotmail.com>
2023-01-10 17:02:09 +0100

    Added execeptions for CharacterNotFound and ItemNotFound.

---
+++ src/TextAdventure.Core/Exceptions/CharacterNotFoundException .cs
@@ -0,0 +1,18 @@
+﻿namespace TextAdventure.Core.Exceptions
+{
+    public class CharacterNotFoundException : Exception
+    {
+        public CharacterNotFoundException()
+        {
+        }
+
+        public CharacterNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public CharacterNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+    }
+}
---
+++ src/TextAdventure.Core/Exceptions/ItemNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace TextAdventure.Core.Exceptions
+{
+    public class ItemNotFoundException : Exception
+    {
+        public ItemNotFoundException()
+        {
+        }
+
+        public ItemNotFoundException(string? message) : base(message)
+        {
+        }
+
+        public ItemNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+    }
+}
