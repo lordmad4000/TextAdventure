921edbc8bf1735e255ac9dfbbac552fbec58276d
joseantonio <lordmad4000@hotmail.com>
2023-01-11 10:13:27 +0100

    Added mediatr DI to application.

---
+++ src/TextAdventure.Application/RegisterServices.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TextAdventure.Application
+{
+    public static class RegisterServices
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(RegisterServices).Assembly);
+
+            return services;
+        }
+    }
+}
