ba6b8bd90b53f42d51cbfced6dee19e9914caea3
joseantonio <lordmad4000@hotmail.com>
2023-01-11 10:13:50 +0100

    Added Automapper configuration to application.

---
+++ src/TextAdventure.Application/Configuration/AutoMapper/ApplicationMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TextAdventure.Application.Adventures.Queries.Execute;
+using TextAdventure.Core.Models;
+
+namespace TextAdventure.Application.Configuration.AutoMapper
+{
+    public class ApplicationMappingProfile : Profile
+    {
+        public ApplicationMappingProfile() 
+        {
+            CreateMap<Adventure, AdventureDto>();
+            CreateMap<AdventureDto, Adventure>();
+        }
+    }
+}
---
+++ src/TextAdventure.Application/Configuration/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace TextAdventure.Application.Configuration.AutoMapper
+{
+    public static class AutoMapperConfig
+    {
+        public static MapperConfiguration RegisterMappings()
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.AllowNullCollections = true;
+                cfg.AllowNullDestinationValues = true;
+                cfg.AddProfile(new ApplicationMappingProfile());
+            });
+        }
+    }
+}
