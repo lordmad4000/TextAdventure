4043b52942da44923fb28c7da6a9c882cd8b1150
joseantonio <lordmad4000@hotmail.com>
2023-01-11 11:03:50 +0100

    Added new directions.

--- src/TextAdventure.Core/Enums/Directions.cs
+++ src/TextAdventure.Core/Enums/Directions.cs
@@ -1,15 +1,15 @@
 public enum Directions
 {
-	North,
-	Northeast,
-	Northwest,
-	South,
-	Southeast,
-	Southwest,
-	East,
-	West,
-	Up,
-	Down,
-	Inside,
-	Outside
+    North,
+    Northeast,
+    Northwest,
+    South,
+    Southeast,
+    Southwest,
+    East,
+    West,
+    Up,
+    Down,
+    Inside,
+    Outside
 }
