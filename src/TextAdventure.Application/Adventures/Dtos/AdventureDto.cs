843cff304f5e108e74ab8c96a38ad568a3052214
joseantonio <lordmad4000@hotmail.com>
2023-01-11 11:12:35 +0100

    Added AdventureDto.

---
+++ src/TextAdventure.Application/Adventures/Dtos/AdventureDto.cs
@@ -0,0 +1,29 @@
+﻿using TextAdventure.Core.Models;
+
+namespace TextAdventure.Application.Adventures.Dtos
+{
+    public class AdventureDto
+    {
+        public AdventureDto(int id, string name, string description, string outputText, Room actualRoom, List<Item> items, List<Character> characters, bool loadRoom)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            OutputText = outputText;
+            ActualRoom = actualRoom;
+            Items = items;
+            Characters = characters;
+            LoadRoom = loadRoom;
+        }
+
+        public int Id { get; }
+        public string Name { get; }
+        public string Description { get; }
+        public string OutputText { get; set; }
+        public Room ActualRoom { get; set; }
+        public List<Item> Items { get; }
+        public List<Character> Characters { get; }
+        public bool LoadRoom { get; }
+
+    }
+}
--- src/TextAdventure.Application/Configuration/AutoMapper/ApplicationMappingProfile.cs
+++ src/TextAdventure.Application/Configuration/AutoMapper/ApplicationMappingProfile.cs
@@ -1,12 +1,12 @@
 ﻿using AutoMapper;
-using TextAdventure.Application.Adventures.Queries.Execute;
+using TextAdventure.Application.Adventures.Dtos;
 using TextAdventure.Core.Models;
 
 namespace TextAdventure.Application.Configuration.AutoMapper
 {
     public class ApplicationMappingProfile : Profile
     {
-        public ApplicationMappingProfile() 
+        public ApplicationMappingProfile()
         {
             CreateMap<Adventure, AdventureDto>();
             CreateMap<AdventureDto, Adventure>();
